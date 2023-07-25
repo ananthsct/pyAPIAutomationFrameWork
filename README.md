26cbeb3f62fc53d85a8d90cb26d55ad065bd8e47
ananthsct <ananthsct@gmail.com>
2023-07-25 09:15:44 +0530

    adding new files to API Automation frmaework

--- README.md
+++ README.md
@@ -1,3 +1,31 @@
-# API Automation
+# Python API Automation Framework
+## Integration Test cases for the Restful Booker
 
-## Documentation
+URL - https://restful-booker.herokuapp.com/apidoc/index.html
+
+1. Verify GET, POST, PACTH, DELETE, PUT
+2. Response Body, Headers, Status Code.
+3. Auth - Basic Auth, Cookie Based Auth.
+4. JSON Schema Validation.
+
+## Tech Stack (Python Packages used)
+
+1. Python, Request Module
+2. PyTest, PyTest-html
+3. Allure Report
+4. Faker, CSV, JSON, YAML.
+5. Run via Commandline - Jenkins
+
+P.S - DB Connection(in future)
+
+## Install pip packages
+1. pip install requests pytest pytest-html faker allure-pytest jsonschema
+2. pip install requirements.txt
+
+## How to run Locally and see the Report?
+
+pytest tests/* -s -v --alluredir=./reports --html=report.html
+
+## How to Run via Jenkins?
+
+https://github.com/PramodDutta/PyAPIAutomationFramwork
---
+++ tests/integration_test/test_create_booking.py
@@ -0,0 +1,58 @@
+'''
+
+Author : Ananth
+Objective : Create Verify by POST Request
+TC#1 - Verify the status Code, Headers
+TC#2 - Verify the Body > Booking ID
+TC#3 - Verify the JSON Schema is valid
+
+Assertion
+Expected Result == Actual Result
+
+'''
+import pytest
+from src.helpers.api_wrapper import *
+from src.constants.apiconstant import base_url, url_create_booking
+from src.helpers.common_verification import *
+from src.helpers.payload_manager import payload_create_booking
+from src.helpers.utils import common_headers
+
+# Payload
+# Base URL
+# Verify
+
+class TestIntegration(object):
+
+    @pytest.fixture(scope="module")
+    def setup(self):
+        print("Before")
+
+    @pytest.mark.run(order=1)
+    def test_create_booking_tc1(self):
+        response = post_request(url=url_create_booking(), headers=common_headers(), auth=None,
+                                payload=payload_create_booking(), in_json=True)
+        verify_http_code(response, 200)
+        verify_key_for_not_null_greater_than_zero(response.json()["bookingid"])
+
+    # URL -> Separate URL
+    # Payload - Separate Payload manager
+    # Headers -> Headers Utils
+    # Verify - Seperate Verify
+
+'''
+
+
+    @pytest.mark.run(order=2)
+    def test_create_booking_tc2():
+        assert True == False
+
+
+    @pytest.mark.run(order=3)
+    def test_create_booking_tc3():
+        assert True == True
+
+
+    def tear_down(self):
+        print("End")
+        
+'''
